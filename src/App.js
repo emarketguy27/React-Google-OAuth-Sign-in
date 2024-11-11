51a52b30aedcb9eac7719182a1a1c622a8b77788
emarketguy27 <j.dennisuk@gmail.com>
2024-11-11 12:45:00 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [userData, setUserData] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>React Google Sign-In</h2>
+      {!userData && (
+        <GoogleLogin
+          className="sign"
+          onSuccess={(credentialResponse) => {
+            const details = jwtDecode(credentialResponse.credential);
+            console.log(details);
+            const userData = {
+              picture: details.picture,
+              name: details.name,
+              email: details.email,
+            };
+            setUserData(userData);
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+      )}
+      {userData && (
+        <div>
+          <h3>Logged in</h3>
+          <div className="flex border b-r">
+            <img src={userData.picture} className="profile-pic" alt="Profile" />
+            <div className="column">
+              <h3>{userData.name}</h3>
+              <p>Email: {userData.email}</p>
+            </div>
+            
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+const oAuthId = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID;
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId={oAuthId}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
